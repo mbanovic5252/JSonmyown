1ce0b97ebd18f6f75505afc33d9c1356c2dd07cd
mbanovic5252 <warlord5252@gmail.com>
2022-10-26 21:30:23 +0200

    quickfix#2

--- blackjack.js
+++ blackjack.js
@@ -1,8 +1,8 @@
 // >>>>>>>>>BLACKJACK<<<<<<<
 let player = {
-    name: "Player",
-    chips: 0
-}
+  name: "Player",
+  chips: 1000,
+};
 let playerEl = document.querySelector("#playerel");
 playerEl.textContent = player.name + ": $" + player.chips;
 let cards = [];
@@ -13,79 +13,83 @@ let message = "";
 let messageEl = document.querySelector("#messageel");
 let sumEl = document.querySelector("#sumel");
 let cardsEl = document.querySelector("#cardsel");
-
-
-
+let diveverythingEl = document.querySelector("#diveverything");
+let betChosen = false;
+let betChooseEl = document.querySelector("#betchooseel");
 
 let btnStart = document.querySelector("#btnstart");
 let btnNewCard = document.querySelector("#btnnewcard");
 let btnStand = document.querySelector("#btnstand");
 let btnReset = document.querySelector("#btnreset");
 
-
-function startGame(){
-    isAlive = true;
-    let firstCard = getRandomCard();
-    let secondCard = getRandomCard();
-    cards = [firstCard, secondCard];
-    sum = firstCard + secondCard;
-    renderGame();
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
 }
 
-function renderGame(){
-    if (sum <= 20){
-        message = "Do you want to draw a new card?";
-        } else if (sum === 21) {
-        message = "You've got blackjack!";
-        hasBlackjack = true;
-        btnReset.style.display="block";
-        } else {
-        message = "You're out of the game!";
-        isAlive = false;
-        btnReset.style.display="block";
-        }
-        messageEl.textContent = message;
-        sumEl.textContent = "Sum: " + sum;
-        cardsEl.textContent = "Cards: "
-        for (let i = 0; i < cards.length; i++){
-            cardsEl.textContent += cards[i] + " ";
-        }      
+function renderGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got blackjack!";
+    hasBlackjack = true;
+    btnReset.style.display = "block";
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+    btnReset.style.display = "block";
+  }
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
 }
 // Upon clicking the button new card, draw a new card from the deck and display it.
-function newCard(){
-    if (isAlive === true && hasBlackjack === false){
-        let newCard = getRandomCard();
-        sum += newCard;
-        cards.push(newCard);
-        renderGame();
-    }
+function newCard() {
+  if (isAlive === true && hasBlackjack === false) {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+  }
 }
 
 // Reset the game upon getting blackjack or hitting 22 or more
-function resetGame(){
-    if (sum >= 21){
-        hasBlackjack = false;
-        isAlive = false;
-        startGame();
-    }   
+function resetGame() {
+  if (sum >= 21) {
+    hasBlackjack = false;
+    isAlive = false;
+    startGame();
+  }
 }
+// Stand button has been clicked.
+function standClicked() {
+  if (sum <= 21) {
+    stand();
+  }
+}
+function stand() {}
 
 // Function generates a random number (card) between 2 and 11.
-function getRandomCard(){
-    return Math.floor(Math.random() *10 + 2);
+function getRandomCard() {
+  return Math.floor(Math.random() * 10 + 2);
 }
 
-// New card button & stand button are hidden up until game starts. Then they are shown and 
+// New card button & stand button are hidden up until game starts. Then they are shown and
 // start button goes hidden.
-btnStart.addEventListener("click", function() {
-    btnNewCard.style.display="block";
-    btnStart.style.display="none";
-    // btnStand.style.display="block";
-});
-btnReset.addEventListener("click", function() {
-    btnReset.style.display="none";
+btnStart.addEventListener("click", function () {
+  btnNewCard.style.display = "block";
+  btnStart.style.display = "none";
+  btnStand.style.display = "block";
 });
 
-
-
-
+//
+btnReset.addEventListener("click", function () {
+  btnReset.style.display = "none";
+});
